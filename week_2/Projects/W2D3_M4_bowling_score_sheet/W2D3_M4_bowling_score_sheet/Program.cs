ea12a3bbbfeb882815d2017c1bc1454bee36dc74
Rymdlingen <71494058+Rymdlingen@users.noreply.github.com>
2020-09-18 23:36:38 +0200

    Organized!
    W2D4_M2_battle_with_the_basilisk is not done. All the others are done but some could probably be improved!

---
+++ week_2/Projects/W2D3_M4_bowling_score_sheet/W2D3_M4_bowling_score_sheet/Program.cs
@@ -0,0 +1,263 @@
+﻿using System;
+
+namespace mission_4_bowling_score_sheet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //firstTry();
+            //secondTry();
+            //emptyScoreSheet();
+            bowling();
+
+        }
+
+        static void bowling()
+        {
+            var random = new Random();
+
+            int roll1;
+            int roll2;
+
+            string roll1Text = "";
+            string roll2Text = "";
+
+            int numberOfRolls = random.Next(1, 11);
+            int rolls = 0;
+
+            string topBot = "";
+            string topBotEnd = "+";
+            string row2 = "";
+            string row2End = "|";
+            string row3 = "";
+            string row3End = "|";
+            string row4 = "";
+            string row4End = "|";
+
+            while (rolls < numberOfRolls)
+            {
+                roll1 = random.Next(0, 11);
+                roll2 = random.Next(0, 11 - roll1);
+
+                if (roll1 < 10)
+                {
+                    if (roll1 == 0)
+                    {
+                        roll1Text += "-";
+                    }
+                    else
+                    {
+                        roll1Text += roll1;
+                    }
+                }
+                else
+                {
+                    roll1Text += "X";
+                    roll2Text += " ";
+                }
+
+                if (roll1 != 10)
+                {
+                    if (roll2 < 10 - roll1)
+                    {
+                        if (roll2 == 0)
+                        {
+                            roll2Text += "-";
+                        }
+                        else
+                        {
+                            roll2Text += roll2;
+                        }
+
+                    }
+                    else
+                    {
+                        roll2Text += "/";
+                    }
+
+                }
+
+                topBot += "+-----";
+                row2 += $"| |{roll1Text}|{roll2Text}";
+                row3 += "| ----";
+                row4 += "|     ";
+
+                rolls++;
+
+                roll1Text = "";
+                roll2Text = "";
+
+            }
+
+            Console.Write(topBot);
+            Console.WriteLine(topBotEnd);
+            Console.Write(row2);
+            Console.WriteLine(row2End);
+            Console.Write(row3);
+            Console.WriteLine(row3End);
+            Console.Write(row4);
+            Console.WriteLine(row4End);
+            Console.Write(topBot);
+            Console.WriteLine(topBotEnd);
+
+            //Console.WriteLine(numberOfRolls);
+        }
+
+        static void secondTry()
+        {
+            var random = new Random();
+            int roll1 = random.Next(0, 11);
+            int roll2 = random.Next(0, 11 - roll1);
+            int knockedPins = roll1 + roll2;
+            string roll1Text = "First roll: ";
+            string roll2Text = "Second roll: ";
+
+            if (roll1 < 10)
+            {
+                if (roll1 == 0)
+                {
+                    roll1Text += "-";
+                }
+                else
+                {
+                    roll1Text += $"{roll1}";
+                }
+            }
+            else
+            {
+                roll1Text += "X";
+            }
+
+            Console.WriteLine(roll1Text);
+
+            if (roll1 != 10)
+            {
+                if (roll2 < 10 - roll1)
+                {
+                    if (roll2 == 0)
+                    {
+                        roll2Text += "-";
+                    }
+                    else
+                    {
+                        roll2Text += $"{roll2}";
+                    }
+
+                }
+                else
+                {
+                    roll2Text += "/";
+                }
+
+                Console.WriteLine(roll2Text);
+            }
+
+
+
+            Console.WriteLine($"Knocked pins: {knockedPins}");
+        }
+
+
+        static void emptyScoreSheet()
+        {
+            var random = new Random();
+            int totalFrames = random.Next(1, 11);
+            string topBot = "+-----";
+            string topBotEnd = "+";
+            string row2 = "| | | ";
+            string row2End = "|";
+            string row3 = "| ----";
+            string row3End = "|";
+            string row4 = "|     ";
+            string row4End = "|";
+
+            for (int loops = 0; loops < totalFrames; loops++)
+            {
+                Console.Write(topBot);
+
+            }
+            Console.WriteLine(topBotEnd);
+
+            for (int loops = 0; loops < totalFrames; loops++)
+            {
+                Console.Write(row2);
+
+            }
+            Console.WriteLine(row2End);
+
+            for (int loops = 0; loops < totalFrames; loops++)
+            {
+                Console.Write(row3);
+
+            }
+            Console.WriteLine(row3End);
+
+            for (int loops = 0; loops < totalFrames; loops++)
+            {
+                Console.Write(row4);
+
+            }
+            Console.WriteLine(row4End);
+
+            for (int loops = 0; loops < totalFrames; loops++)
+            {
+                Console.Write(topBot);
+
+            }
+            Console.WriteLine(topBotEnd);
+
+            Console.WriteLine(totalFrames);
+        }
+
+
+        static void firstTry()
+        {
+
+            var random = new Random();
+            int roll1 = random.Next(0, 11);
+            int roll2 = random.Next(0, 11 - roll1);
+            int knockedPins = roll1 + roll2;
+
+            if (roll1 < 10)
+            {
+                if (roll2 != 10 - roll1)
+                {
+                    if (roll1 == 0 || roll2 == 0)
+                    {
+                        if (roll1 == 0)
+                        {
+                            Console.WriteLine("First roll: -");
+                            Console.WriteLine($"Second roll: {roll2}");
+                            Console.WriteLine($"Knocked pins: {knockedPins}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"First roll: {roll1}");
+                            Console.WriteLine($"Second roll: -");
+                            Console.WriteLine($"Knocked pins: {knockedPins}");
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"First roll: {roll1}");
+                        Console.WriteLine($"Second roll: {roll2}");
+                        Console.WriteLine($"Knocked pins: {knockedPins}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"First roll: {roll1}");
+                    Console.WriteLine("Second roll: /");
+                    Console.WriteLine($"Knocked pins: {knockedPins}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("First roll: X");
+                Console.WriteLine($"Knocked pins: {knockedPins}");
+            }
+        }
+    }
+}
---
+++ week_2/Projects/W2D4_M2_battle_with_the_basilisk/W2D4_M2_battle_with_the_basilisk/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace W2D4_mission_2_battle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------------");
+            var party = new List<string> { "Johanna", "Sandra", "Linn", "Sallie" };
+            var partyString = String.Join(", ", party);
+
+            Console.WriteLine($"Four brave warriors ({partyString}) are out on an adventure. When they have been wandering a while without much excitement they notice a cave. {party[0]} challanges the others to enter. 'I'm not scared!' says {party[1]}, 'Me neither!' {party[2]} agrees. '....voff' says {party[3]} and into the cave they go!");
+            Console.WriteLine("The brave adventurers explore the cave for hours and in an unusally dark and big room they encounter a terrifying basilisk.");
+
+            var random = new Random();
+            int dice8 = 0;
+            int HP = 16;
+
+            for (var rolls = 0; rolls < 8; rolls++)
+            {
+                dice8 = random.Next(1, 9);
+                //Console.WriteLine(dice8);
+                HP += dice8;
+            }
+
+            Console.WriteLine($"The basilisk has {HP} HP.");
+            Console.WriteLine($"The three sisters simultanusly draw their greatswords, {party[3]} growls and looks ready to attack.");
+            Console.WriteLine();
+
+            int dice6 = 0;
+            int damage = 0;
+            string lastHit = "";
+
+            while (HP > 0)
+            {
+                for (var i = 0; i < party.Count; i++)
+                {
+                    if (HP > 0)
+                    {
+                        for (var rolls = 0; rolls < 2; rolls++)
+                        {
+                            dice6 = random.Next(1, 7);
+                            //Console.WriteLine(dice6);
+                            damage += dice6;
+                        }
+
+                        HP -= damage;
+
+                        if (HP < 1)
+                        {
+                            HP = 0;
+                            lastHit = String.Join(", ", party[i]);
+                        }
+
+                        Console.WriteLine($"{party[i]} hits the basilisk for {damage} damage. Basilisk has {HP} HP left.");
+
+                        damage = 0;
+
+
+                    }
+                }
+            }
+
+            Console.WriteLine($"{lastHit} hit the basilisk back to the deapths of the cave and all of our heroes safely make it out to the surface again. They were celebrated for weeks, becuse noone had before achived what they had, noone hade before faced the mighty basilisk and lived to tell about it! ... ");
+
+            Console.WriteLine();
+            Console.WriteLine($"{party[0]} wakes up from her daydream about defensless basilisks and greatswords. She heres {party[3]} barks and makes our adventurers aware of a sign that reads \"***Danger***\". With the daydreams fresh in her mind {party[0]} tells the others about all the glory that might await them and convinces them to venture into the cave.");
+
+            HP = 16;
+
+            for (var rolls = 0; rolls < 8; rolls++)
+            {
+                dice8 = random.Next(1, 9);
+                //Console.WriteLine(dice8);
+                HP += dice8;
+            }
+
+            Console.WriteLine($"The basilisk has {HP} HP.");
+
+            int dice4;
+            int partyIndex;
+            int dice20;
+            int constitution;
+
+            while (HP > 0)
+            {
+                for (var i = 0; i < party.Count; i++)
+                {
+                    if (HP > 0)
+                    {
+
+                        dice4 = random.Next(1, 5);
+                        //Console.WriteLine(dice4);
+                        damage = dice4;
+
+                        HP -= damage;
+
+                        if (HP < 0)
+                        {
+                            HP = 0;
+                            lastHit = String.Join(", ", party[i]);
+                        }
+
+                        Console.WriteLine($"{party[i]} hits the basilisk for {damage} damage. Basilisk has {HP} HP left.");
+
+                        if (i == party.Count - 1 && HP > 0)
+                        {
+                            constitution = 5;
+                            partyIndex = random.Next(0, party.Count);
+
+                            Console.WriteLine($"The basilisk stares at {party[partyIndex]}, it prepares it's petrifying gaze and {party[partyIndex]} starts to run to get out of it's reach");
+
+                            dice20 = random.Next(1, 21);
+                            constitution += dice20;
+
+                            Console.Write($"{party[partyIndex]} runs {dice20} steps away ");
+                            if (constitution < 12)
+                            {
+                                Console.WriteLine($"but the basilisk range is greter than that and she quickly gets turned into stone.");
+                                party.Remove(party[partyIndex]);
+                            }
+                            else
+                            {
+                                Console.WriteLine($"and makes it out of the basilisks range. Our hero manages to avoid beeing turned into stone.");
+                            }
+
+                            if (party.Count == 0)
+                            {
+                                Console.WriteLine("All of our heroes are now turned into stone and the story ends here. Noone lives to tell the story of our adventurers heroic fight and ?? defeat.");
+                            }
+
+                            //Console.WriteLine(party.Count);
+                        }
+
+                        if (HP == 0)
+                        {
+                            Console.WriteLine($"Hurray! Our heroes made it! They have defeated the basilisk and returns to their village. ");
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+//Johanna wakes up from her daydreams about defensless basilisks and greatswords...
+//they are saved if they manage to run 12 or more steps away before the gaze hits
+//något om att den fjärde är en hund?
+//väldigt osanolik grej om att de hittar något blankt och basilisken förstenar sig själv?
