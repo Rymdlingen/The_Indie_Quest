61024389ebab7bd24ae0b9aadf79c463bc5e1327
Rymdlingen <71494058+Rymdlingen@users.noreply.github.com>
2020-10-01 11:45:48 +0200

    Party_dilemma

---
+++ week_3/Party_dilemma/Party_dilemma/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Party_dilemma
+{
+    class Program
+    {
+        static int Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            for (int factorial = 0; factorial < 11; factorial++)
+            {
+                Console.WriteLine($"{factorial}! = {Factorial(factorial)}");
+            }
+        }
+    }
+}
