583a73862d2bf6e9b28e6a1448f1299d5f8da1b3
Rymdlingen <71494058+Rymdlingen@users.noreply.github.com>
2020-10-07 18:19:20 +0200

    Part 2 of the mission is done!

--- week_5/Full_bowling_score_sheet/Full_bowling_score_sheet/Program.cs
+++ week_5/Full_bowling_score_sheet/Full_bowling_score_sheet/Program.cs
@@ -47,6 +47,57 @@ namespace W3D1_bowling_pins
                 }
             }
 
+            // Calculating gained points for each frame
+            int[] pointsGained = new int[10];
+
+            for (int frame = 0; frame <= allRolls.Length - 1; frame++)
+            {
+                for (int rolls = 0; rolls <= allRolls[frame].Length - 1; rolls++)
+                {
+                    pointsGained[frame] += allRolls[frame][rolls];
+
+                    // If it's the last frame this will just add the last rolls together
+                    // and then skip the rest of the calculations, since they don't need to happen
+                    if (frame == 9)
+                    {
+                        continue;
+                    }
+
+                    // If you get a strike
+                    if (allRolls[frame].Length == 1)
+                    {
+                        if (allRolls[frame + 1].Length == 2 || allRolls[frame + 1].Length == 3)
+                        {
+                            pointsGained[frame] += allRolls[frame + 1][rolls];
+                            pointsGained[frame] += allRolls[frame + 1][rolls + 1];
+                        }
+                        else
+                        {
+                            pointsGained[frame] += allRolls[frame + 1][rolls];
+                            pointsGained[frame] += allRolls[frame + 2][rolls];
+                        }
+                    }
+
+                    // If you get a spare
+                    if (rolls == 1 && allRolls[frame][rolls - 1] + allRolls[frame][rolls] == 10)
+                    {
+                        pointsGained[frame] += allRolls[frame + 1][0];
+                    }
+
+                }
+            }
+
+            // Calculating the total score (and storing the score from each frame)
+
+            int[] frameScores = new int[10];
+
+            frameScores[0] = pointsGained[0];
+
+            for (int frame = 1; frame < pointsGained.Length; frame++)
+            {
+                frameScores[frame] = frameScores[frame - 1] + pointsGained[frame];
+            }
+
             // Displaying my array
             for (int frame = 0; frame < allRolls.Length; frame++)
             {
@@ -154,6 +205,8 @@ namespace W3D1_bowling_pins
                     }
                 }
                 Console.WriteLine($"Knocked pins: {knockedPins}");
+                Console.WriteLine($"Points gained: {pointsGained[frame]}");
+                Console.WriteLine($"Score: {frameScores[frame]}");
                 Console.WriteLine();
             }
         }
