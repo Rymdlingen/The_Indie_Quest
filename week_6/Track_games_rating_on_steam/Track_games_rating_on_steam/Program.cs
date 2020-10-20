ead33671bfb606542182c812d1e5f10989f744d0
Rymdlingen <71494058+Rymdlingen@users.noreply.github.com>
2020-10-20 20:53:56 +0200

    Updated to check more games, extract names and 0-2 ratings

--- week_6/Track_games_rating_on_steam/Track_games_rating_on_steam/Program.cs
+++ week_6/Track_games_rating_on_steam/Track_games_rating_on_steam/Program.cs
@@ -11,20 +11,71 @@ namespace Track_games_rating_on_steam
         {
             // Getting the HTML code from the website
             var httpClient = new HttpClient();
-            string htmlCode = httpClient.GetStringAsync(@"https://store.steampowered.com/app/413150/Stardew_Valley/").Result;
+            // A bunch of games I like and random games with different rankings
+            string[] htmlCode = new string[] { httpClient.GetStringAsync(@"https://store.steampowered.com/app/413150/Stardew_Valley/").Result,
+                                               httpClient.GetStringAsync(@"https://store.steampowered.com/app/232790/Broken_Age/").Result,
+                                               httpClient.GetStringAsync(@"https://store.steampowered.com/app/224760/FEZ/").Result,
+                                               httpClient.GetStringAsync(@"https://store.steampowered.com/app/609490/Minit/").Result,
+                                               httpClient.GetStringAsync(@"https://store.steampowered.com/app/233470/Evoland/").Result,
+                                               httpClient.GetStringAsync(@"https://store.steampowered.com/app/339240/10_Years_After/").Result,
+                                               httpClient.GetStringAsync(@"https://store.steampowered.com/app/223598/The_Sims_3_Island_Paradise/").Result,
+                                               httpClient.GetStringAsync(@"https://store.steampowered.com/app/606860/Nephise/?curator_clanid=11846936").Result,
+                                               httpClient.GetStringAsync(@"https://store.steampowered.com/app/47400/Stronghold_3_Gold/").Result,
+                                               httpClient.GetStringAsync(@"https://store.steampowered.com/app/1235722/The_Sims_4_Discover_University/").Result,
+                                               httpClient.GetStringAsync(@"https://store.steampowered.com/app/513710/SCUM/").Result,
+            };
 
-            // Pattern for extracting the rating
-            string ratingPattern = @"<.*game_review_summary.*description.*>(.*)<\/span>";
-            //string ratingPattern = @"<.*description.*>(.*)<.*>";
-            // The pattern above also works
+            // Pattern for extracting the name of the game
+            string namePattern = @"<b>Title:</b> (.*)<br>";
+            // Pattern for extracting all the ratings (0-2)
+            string ratingPattern = @"<.*game_review_summary.*data.*>([\D].*)<\/span>";
+            //old pattern <.*game_review_summary.*>(.*)<\/span>
 
-            // Putting the extracted rating in a string
-            Match match = Regex.Match(htmlCode, ratingPattern);
-            GroupCollection groups = match.Groups;
-            string rating = groups[1].Value;
+            // Putting the extracted names and ratings in an array
+            string[][] namesAndRatings = new string[htmlCode.Length][];
+            for (int length = 0; length < namesAndRatings.Length; length++)
+            {
+                namesAndRatings[length] = new string[3];
+            }
+
+            // Checking one link at the time
+            for (int code = 0; code < htmlCode.Length; code++)
+            {
+                Match name = Regex.Match(htmlCode[code], namePattern);
+                MatchCollection ratings = Regex.Matches(htmlCode[code], ratingPattern);
+                GroupCollection groups;
+
+                // Adding the name to the first spot in the array
+                groups = name.Groups;
+                namesAndRatings[code][0] = groups[1].Value;
+
+                // Adding the second group (only the text) of every match to the array
+                for (int rating = 0; rating < ratings.Count; rating++)
+                {
+                    groups = ratings[rating].Groups;
+                    namesAndRatings[code][rating + 1] = groups[1].Value;
+                }
+            }
 
             // Output
-            Console.WriteLine($"The rating ofthe game Stardew Valley is {rating}.");
+            for (int game = 0; game < namesAndRatings.Length; game++)
+            {
+                Console.WriteLine(namesAndRatings[game][0].ToUpper());
+                if (namesAndRatings[game][2] != null)
+                {
+                    Console.WriteLine($"Recent reviews: {namesAndRatings[game][2]}");
+                    Console.WriteLine($"All reviews: {namesAndRatings[game][1]}");
+                }
+                else if (namesAndRatings[game][1] != null)
+                {
+                    Console.WriteLine($"All reviews: {namesAndRatings[game][1]}");
+                }
+                else
+                {
+                    Console.WriteLine("This game has no rating.");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
