3698adc60e11d68584d236db2788701be16ebd6c
Rymdlingen <71494058+Rymdlingen@users.noreply.github.com>
2020-10-21 11:11:35 +0200

    Added method for drawing map
    Added colors, player symbol and threes

--- week_6/Minotaurs_lair/Minotaurs_lair/Program.cs
+++ week_6/Minotaurs_lair/Minotaurs_lair/Program.cs
@@ -6,6 +6,63 @@ namespace Minotaurs_lair
 {
     class Program
     {
+        static int width;
+        static int height;
+        static char[,] map;
+
+        // Variables for saving S(start) position
+        static int startX;
+        static int startY;
+
+        static Random random = new Random();
+
+        // Method for drawing the map
+        static void DrawMap()
+        {
+            // Drawing every character of the map
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    // Color for walls
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                    // Randomly generating forest in the first three rows
+                    if (y < 3)
+                    {
+                        if (random.Next(3 + y * 2) == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            Console.Write("♠");
+                            continue;
+                        }
+                    }
+
+                    // Changing the color for the minotaur
+                    if (map[x, y] == 'M')
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                    }
+
+                    // Drawing the character for this coordinate
+                    Console.Write(map[x, y]);
+                }
+                // New line
+                Console.WriteLine();
+            }
+
+            // Drawing the player at the start position
+            Console.CursorTop = startY;
+            Console.CursorLeft = startX;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("☺");
+
+            // Setting the cursor at the bottom and the color back to gray
+            Console.CursorTop = height;
+            Console.CursorLeft = 0;
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
         static void Main(string[] args)
         {
             // Extracting all lines from the text file
@@ -21,33 +78,35 @@ namespace Minotaurs_lair
             GroupCollection xAndY = sizes.Groups;
 
             // Dimensions (x, widthn and y, height)
-            int x = Int32.Parse(xAndY[1].Value);
-            int y = Int32.Parse(xAndY[2].Value);
-
-            // Variables for saving S(start) position
-            int startX;
-            int startY;
+            width = Int32.Parse(xAndY[1].Value);
+            height = Int32.Parse(xAndY[2].Value);
 
             // Storing level information int an 2-dimensional array
-            char[,] level = new char[x, y];
-            for (int height = 0; height < y; height++)
+            map = new char[width, height];
+            for (int y = 0; y < height; y++)
             {
                 // Storing all the data on this height
-                string currentRow = mazeLevelText[height + 2];
+                string currentRow = mazeLevelText[y + 2];
 
-                for (int width = 0; width < x; width++)
+                for (int x = 0; x < width; x++)
                 {
-                    // Extarcting data from ever x on this y
-                    level[width, height] = currentRow[width];
-
-                    // Checking for start position and storing the coordinates
-                    if (currentRow[width] == 'S')
+                    // Checking for start position, storing the coordinates and setting the map char to space
+                    if (currentRow[x] == 'S')
                     {
-                        startX = width;
-                        startY = height;
+                        map[x, y] = ' ';
+                        startX = x;
+                        startY = y;
+                    }
+                    else
+                    {
+                        // Extarcting data from ever x on this y and storing it in the array
+                        map[x, y] = currentRow[x];
                     }
                 }
             }
+
+            // Drawing!
+            DrawMap();
         }
     }
 }
