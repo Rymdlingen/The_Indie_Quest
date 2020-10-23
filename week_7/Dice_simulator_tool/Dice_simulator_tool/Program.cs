17119fba6a0b2bb6d73d7643ab4188db2f8219c2
Rymdlingen <71494058+Rymdlingen@users.noreply.github.com>
2020-10-23 08:28:49 +0200

    Start of Dice_simulator_tool

---
+++ week_7/Dice_simulator_tool/Dice_simulator_tool/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Dice_simulator_tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DICE SIMULATOR\n");
+            Console.WriteLine("Enter desired dice roll in standard dice notation:");
+            Console.ReadLine();
+        }
+    }
+}
